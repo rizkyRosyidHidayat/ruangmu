e76020cba9734ecedb7d15103a6f791c7c8ca71c
Lingga Resti <rizky.hidayat@students.amikom.ac.id>
2019-02-25 08:41:56 +0700

    Updates

---
+++ precache-manifest.4c4ddd4d25766f8acd3e38cf13b1f0a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "74e62b244daf688d5fc44be4d6de243f",
+    "url": "/ruangmu/static/media/nyaman.74e62b24.jpg"
+  },
+  {
+    "revision": "8407db373f45b719a75531c290c72818",
+    "url": "/ruangmu/static/media/mitra.8407db37.jpg"
+  },
+  {
+    "revision": "56b5c3a50317aab8899b4deeb4c833b3",
+    "url": "/ruangmu/static/media/ivana-cajina-316246-unsplash.56b5c3a5.jpg"
+  },
+  {
+    "revision": "c060a9919f682e0e4569c250f091a43d",
+    "url": "/ruangmu/static/media/brooke_cagle_193476__OSqIA.c060a991.jpg"
+  },
+  {
+    "revision": "298d9bc4aba48451c88e",
+    "url": "/ruangmu/static/js/runtime~main.298d9bc4.js"
+  },
+  {
+    "revision": "6c8b638f411f643bbb01",
+    "url": "/ruangmu/static/js/main.6c8b638f.chunk.js"
+  },
+  {
+    "revision": "d2787b98fc3c25dd8ffc",
+    "url": "/ruangmu/static/js/2.d2787b98.chunk.js"
+  },
+  {
+    "revision": "6c8b638f411f643bbb01",
+    "url": "/ruangmu/static/css/main.2b2bacb9.chunk.css"
+  },
+  {
+    "revision": "d2787b98fc3c25dd8ffc",
+    "url": "/ruangmu/static/css/2.7554603f.chunk.css"
+  },
+  {
+    "revision": "0a1e2fa411e933af96d62496ec59aab4",
+    "url": "/ruangmu/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ruangmu/precache-manifest.4c4ddd4d25766f8acd3e38cf13b1f0a0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ruangmu/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
